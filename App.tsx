7741fe4bf1d9961318daa5be50ca72106d4c3a7e
t-kojima <t-kojima@sonicgarden.jp>
2020-11-21 17:06:38 +0900

    init firebase

--- App.tsx
+++ App.tsx
@@ -1,12 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Open up App.tsx to start working on your ap...p!</Text>
       <StatusBar style="auto" />
+      <Button
+        title="Click Me"
+        onPress={() => {
+          alert('アラート表示');
+          console.log('コンソールログ');
+        }}
+      />
     </View>
   );
 }
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # pedometer-eb
+
+ソザイ
+http://rpgdot3319.g1.xrea.com/
