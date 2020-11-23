7c807a3dc80164ef8192b2c72fa5b1340c1bc010
t-kojima <t-kojima@sonicgarden.jp>
2020-11-23 22:59:04 +0900

    add: マミー

--- scripts/add-unit.ts
+++ scripts/add-unit.ts
@@ -8,28 +8,28 @@ import { Unit } from '../src/models';
 admin.initializeApp({ projectId: 'pedometer-eb-staging' });
 
 const unit = new Unit({
-  name: 'ドライアド',
+  name: 'マミー',
   rank: 2,
-  element: '土',
-  type: '精霊',
-  life: 17,
-  atk: 3,
-  def: 16,
-  skillSlots: 1,
+  element: '魔',
+  type: 'アンデッド',
+  life: 34,
+  atk: 10,
+  def: 18,
+  skillSlots: 2,
   itemSlots: 0,
-  spellSlots: ['土', '無'],
+  spellSlots: [],
   image: {
     uri:
-      'https://firebasestorage.googleapis.com/v0/b/pedometer-eb-staging.appspot.com/o/units%2Fmon_028.png?alt=media&token=56d1da43-8746-486a-9cee-11153f276bc4',
-    width: 0,
-    height: 0,
+      'https://firebasestorage.googleapis.com/v0/b/pedometer-eb-staging.appspot.com/o/units%2Fmon_072.png?alt=media&token=f9e39397-b6f1-493c-9cab-b0d07a721c46',
+    width: 72,
+    height: 72,
   },
   isLot: true,
-  rarity: 'rare',
+  rarity: 'normal',
   createdAt: admin.firestore.FieldValue.serverTimestamp() as firebase.firestore.Timestamp,
   updatedAt: admin.firestore.FieldValue.serverTimestamp() as firebase.firestore.Timestamp,
 });
-admin.firestore().collection('units').doc('mon_028').set(unit.toObject()).catch(console.error);
+admin.firestore().collection('units').doc('mon_072').set(unit.toObject()).catch(console.error);
 
 // rank * 30 = base
 // skill = 20
--- src/models/const.ts
+++ src/models/const.ts
@@ -1,19 +1,19 @@
 export type Element = '火' | '水' | '風' | '土' | '聖' | '魔' | '無';
-export type DamageElement = '火炎' | '津波' | '竜巻' | '吹雪' | '電撃' | '地震'
+export type DamageElement = '火炎' | '津波' | '竜巻' | '吹雪' | '電撃' | '地震';
 export type UnitType =
   | '魔法生物'
-  | 'angel'
-  | 'dragon'
+  | '天使'
+  | 'ドラゴン'
   | '人間'
-  | 'animal'
-  | 'minotaur' // ミノタウロス
-  | 'demon'
+  | '動物'
+  | 'ミノタウロス'
+  | '悪魔'
   | '精霊'
-  | 'undead'
-  | 'insect'
-  | 'bird'
-  | 'elf'
+  | 'アンデッド'
+  | '昆虫'
+  | '鳥'
+  | 'エルフ'
   | 'リザード'
-  | 'naga' // ナーガ
-  | 'aquarium' // 水生生物
+  | 'ナーガ'
+  | '水生生物'
   | 'unknown';
